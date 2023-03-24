7d1f8321f844614d20664e6d40f64dea3abe9ee6
Youssef El Aissaoui <90868691+YusufLisawi@users.noreply.github.com>
2023-03-24 13:34:52 +0000

    Adding redirect handling

--- sources/parser/parser.c
+++ sources/parser/parser.c
@@ -6,11 +6,59 @@
 /*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 12:42:52 by yelaissa          #+#    #+#             */
-/*   Updated: 2023/03/24 13:22:28 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/03/24 13:34:35 by yelaissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "parser.h"
+
+void	handle_redirects(t_command *cmd, char **args)
+{
+	int	i;
+
+	i = 0;
+	while (args[i])
+	{
+		if (ft_strncmp(args[i], ">", 2) == 0)
+		{
+			if (args[i + 1])
+				cmd->output_redirect = ft_strdup(args[i + 1]);
+			else
+				return ;
+			args[i] = 0;
+			args[i + 1] = 0;
+		}
+		else if (ft_strncmp(args[i], ">>", 2) == 0)
+		{
+			if (args[i + 1])
+				cmd->output_redirect = ft_strjoin(args[i + 1], ">>");
+			else
+				return ;
+			args[i] = 0;
+			args[i + 1] = 0;
+		}
+		else if (ft_strncmp(args[i], "<", 2) == 0)
+		{
+			if (args[i + 1])
+				cmd->input_redirect = ft_strdup(args[i + 1]);
+			else
+				return ;
+			args[i] = 0;
+			args[i + 1] = 0;
+		}
+		else if (ft_strncmp(args[i], "<<", 2) == 0)
+		{
+			if (args[i + 1])
+				cmd->input_redirect = ft_strjoin(args[i + 1], "<<");
+			else
+				return ;
+			args[i] = 0;
+			args[i + 1] = 0;
+		}
+		i++;
+	}
+}
+
 
 t_command	init_cmd(char *input)
 {
