e1573c46d75b67e94c4614269df542192b7578e3
hamzatlh <htlhaouia@gmail.com>
2023-03-25 20:34:01 +0000

    added execution command , still working on issue in Makefile

--- sources/execute/validation.c
+++ sources/execute/validation.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   validation.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/24 21:50:33 by htalhaou          #+#    #+#             */
-/*   Updated: 2023/03/25 15:23:20 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/03/25 20:10:11 by htalhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	verify_input(t_shell *shell, char *str)
 	char	**command;
 	char	*path;
 	char	**first_cmd;
+	int		status;
+	pid_t	pid;
 	int		i;
 
 	command = ft_split(shell->path, ':');
@@ -29,13 +31,30 @@ int	verify_input(t_shell *shell, char *str)
 		if (access(path, F_OK) == 0 || !ft_strcmp(path, "export") \
 			|| !ft_strcmp(path, "unset"))
 		{
-			free(path);
-			return (1);
+			pid = fork();
+			if (pid == -1)
+			{
+			  printf("%s\n", strerror(errno));
+			  return (-1);
+			}
+			if (pid == 0)
+			{
+				if (execve(path, first_cmd, NULL) == -1)
+				{
+					printf("%s\n", strerror(errno));
+					return (-1);
+				}
+			}
+			else
+			{
+				waitpid(pid, &status, 0);
+				return (0);
+			}
 		}
 		free(path);
 		i++;
 	}
 	printf(BRED"→  "CX "minishell: command not found: " BRED"%s\n"CX, \
-		first_cmd[0]);
+	first_cmd[0]);
 	return (free_split(first_cmd), free_split(command), 0);
 }
