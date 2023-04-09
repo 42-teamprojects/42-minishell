acca3ebf1b9ecf703de777d82b89ff15711e8145
Youssef El Aissaoui <90868691+YusufLisawi@users.noreply.github.com>
2023-04-09 15:51:06 +0000

    Updates

--- sources/execute/ft_cd.c
+++ sources/execute/ft_cd.c
@@ -6,7 +6,7 @@
 /*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/08 23:25:30 by htalhaou          #+#    #+#             */
-/*   Updated: 2023/04/09 15:08:36 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/04/09 15:50:22 by yelaissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,20 +22,16 @@ int	ft_cd(t_shell **shell)
 	{
 		if (ft_getenv(shell, "OLDPWD") == NULL)
 			return (console(0, "cd", "OLDPWD not set"), 0);
-		chdir(ft_getenv(shell, "OLDPWD"));
-		console(1, "cd", ft_getenv(shell, "OLDPWD"));
-		ft_setenv("OLDPWD", s, shell);
-		return (0);
+		return (chdir(ft_getenv(shell, "OLDPWD")), \
+			console(1, "cd", ft_getenv(shell, "OLDPWD")), \
+			ft_setenv("OLDPWD", s, shell), 0);
 	}
 	if ((*shell)->cmds[0]->args[0] == NULL || \
 		*((*shell)->cmds[0]->args[0]) == '~')
-	{
-		ft_setenv("OLDPWD", s, shell);
-		chdir(ft_strjoin("/Users/", getenv("USER")));
-		return (0);
-	}
+		return (ft_setenv("OLDPWD", s, shell), \
+			chdir(ft_strjoin("/Users/", getenv("USER"))), 0);
 	ft_setenv("OLDPWD", s, shell);
 	if (chdir((*shell)->cmds[0]->args[0]) != 0)
-		return (console(0, "cd", strerror(errno)), 1);
+		return (console(0, (*shell)->cmds[0]->args[0], strerror(errno)), 1);
 	return (ft_setenv("PWD", getcwd(s, sizeof(s)), shell), 0);
 }
