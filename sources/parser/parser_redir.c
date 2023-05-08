9443764d1aeca98a30f2a369bf4eeb1927474d9d
hamzatlh <htlhaouia@gmail.com>
2023-05-08 17:04:09 +0100

    42/74 test in redirections

--- sources/parser/parser_redir.c
+++ sources/parser/parser_redir.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parser_redir.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
+/*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/16 21:26:32 by yelaissa          #+#    #+#             */
-/*   Updated: 2023/05/07 22:49:11 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/05/08 17:03:27 by htalhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -80,48 +80,70 @@ char	*open_heredoc(t_lexer **tokens, t_shell **shell)
 // 	rd_addback(rd, new_rd(file, type));
 // 	return (0);
 // }
-int	handle_redir(t_rd **rd, t_lexer **tokens, t_shell **shell)
-{
-	char			**args;
-	int				i;
-	t_token_type	type;
-	char			*file;
-	t_lexer			*tmp;
 
-	type = (*tokens)->token->type;
-	while ((*tokens)->token->type != WORD && (*tokens)->token->type != VAR \
-		&& (*tokens)->token->type != SQUOTE && (*tokens)->token->type != DQUOTE)
-		*tokens = (*tokens)->next;
-	if (type == HEREDOC)
-		file = open_heredoc(tokens, shell);
-	else
-	{
-		i = 0;
-		args = (char **)malloc(sizeof(char *) * (args_len(*tokens, shell, WSPACE) + 1));
-		if (!args)
-			return (1);
-		tmp = *tokens;
-		while ((*tokens))
-		{
-			if (is_word(*tokens))
-				handle_word(args, &i, tokens, shell);
-			if (is_quote(*tokens))
-				handle_quote(args, &i, tokens, shell);
-			if ((*tokens)->token->type == WSPACE || !(*tokens)->next)
-				break ;
-			(*tokens) = (*tokens)->next;
-		}
-		args[i] = NULL;
-		if (args_count(args) == 0 || args_count(args) > 1 || !args[0] || !ft_strlen(args[0]) || !ft_strcmp(args[0], ""))
-		{
-			(*shell)->status_code = 1;
-			return (stop(-1, shell), console(1, "", "ambiguous redirect"), 1);
-		}
-		file = ft_strdup(args[0]);
-	}
-	rd_addback(rd, new_rd(file, type));
-	return (0);
+int validate_file_name(const char *file_name)
+{
+    if (file_name == NULL || *file_name == '\0')
+        return (1);
+    const char *invalid_chars = "/\\?%*:|\"<>";
+	if (ft_strchr(invalid_chars, *file_name) != NULL || !ft_isprint(*file_name))
+		return (1);
+    size_t max_file_name_length = 255;
+    if (strlen(file_name) > max_file_name_length)
+        return (1);
+    return (0);
 }
+
+int handle_redir(t_rd **rd, t_lexer **tokens, t_shell **shell)
+{
+    char **args;
+    int i;
+    t_token_type type;
+    char *file;
+    t_lexer *tmp;
+
+    type = (*tokens)->token->type;
+    while ((*tokens)->token->type != WORD && (*tokens)->token->type != VAR &&
+           (*tokens)->token->type != SQUOTE && (*tokens)->token->type != DQUOTE)
+        *tokens = (*tokens)->next;
+    if (type == HEREDOC)
+        file = open_heredoc(tokens, shell);
+    else
+    {
+        i = 0;
+        args = (char **)malloc(sizeof(char *) * (args_len(*tokens, shell, WSPACE) + 1));
+        if (!args)
+            return (1);
+        tmp = *tokens;
+        while ((*tokens))
+        {
+            if (is_word(*tokens))
+                handle_word(args, &i, tokens, shell);
+            if (is_quote(*tokens))
+                handle_quote(args, &i, tokens, shell);
+            if ((*tokens)->token->type == WSPACE || !(*tokens)->next)
+                break;
+            (*tokens) = (*tokens)->next;
+        }
+        args[i] = NULL;
+		// printf("==>%s\n", args[0]);
+		if (type == RD_IN && args[0] && args_count(args) == 1) {
+			int fd = open(file, O_RDONLY);
+			if (fd < 0)
+				return (stop(-1, shell), console(1, args[0], strerror(errno)), 1);
+		}
+        if (args_count(args) == 0 || args_count(args) > 1 || !args[0] || !ft_strlen(args[0]) || !ft_strcmp(args[0], "") ||
+            validate_file_name(args[0]))
+        {
+            (*shell)->status_code = 1;
+            return (stop(-1, shell), console(1, "", "ambiguous redirect"), 1);
+        }
+        file = ft_strdup(args[0]);
+    }
+    rd_addback(rd, new_rd(file, type));
+    return (0);
+}
+
 
 void	open_file(char *file, t_token_type type)
 {
