8beb8ae2d5bc753c99316e4972b8bff71d84e2b1
hamzatlh <htlhaouia@gmail.com>
2023-05-10 22:58:02 +0100

    91.57

--- sources/parser/redir_utils.c
+++ sources/parser/redir_utils.c
@@ -6,7 +6,7 @@
 /*   By: htalhaou <htalhaou@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/10 15:13:56 by htalhaou          #+#    #+#             */
-/*   Updated: 2023/05/10 18:01:18 by htalhaou         ###   ########.fr       */
+/*   Updated: 2023/05/10 22:54:32 by htalhaou         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -47,41 +47,43 @@ int	handle_word_redir(char **command, int *i, t_lexer **tokens, t_shell **shell)
 				(*tokens)->token->content + 1), " ");
 			if (!expanded)
 			{
-				// printf("here 1\");	
+				// printf("here 1\n");
 				return (1);
 			}
 			else
 			{
-				if (!(*tokens) && (*tokens)->next == NULL && (*tokens)->prev != NULL)
+				if (ft_strlen(ft_strtrim(expanded, " ")) == 0) //space
 				{
-						char *tmp = ft_strtrim(ft_getenv(shell, (*tokens)->next->token->content + 1), " ");
-						if (tmp && ft_strlen(tmp) == 0)
-						{
-							// printf("hee 2\n");	
-							free(tmp);
-						}
-						else if (tmp && ft_strlen(tmp) != 0)
-						{
-							// printf("her 3\n");	
-							return (1);
-						}
-				}
-				else if (ft_strlen(ft_strtrim(expanded, " ")) == 0 && (*tokens)->prev && (*tokens)->next == NULL)
-				{
-					return (0);
-				}
-				else if (*expanded == ' ' && (*tokens)->prev)
-				{
-					if ((*tokens)->prev->prev && ((*tokens)->prev->prev->token->type != DQUOTE || (*tokens)->prev->prev->token->type != SQUOTE))
+					if (!(*tokens)->next && !(*tokens)->prev)
 						return (1);
+					if ((*tokens)->prev && ((*tokens)->prev->token->type == SQUOTE || (*tokens)->prev->token->type == DQUOTE) \
+					&& ((*tokens)->prev->prev && ((*tokens)->prev->prev->token->type == (*tokens)->prev->token->type)))
+						return (1);
+					if ((*tokens)->next && ((*tokens)->next->token->type == SQUOTE || (*tokens)->next->token->type == DQUOTE) \
+					&& ((*tokens)->next->next && ((*tokens)->next->next->token->type == (*tokens)->next->token->type)))
+						return (1);
+					if ((*tokens)->prev && ((*tokens)->next == NULL || ((*tokens)->next->token->type == WSPACE && (*tokens)->next->token->state == DEFAULT)))
+						return (0);
 				}
-				else if (expanded[ft_strlen(expanded) - 1] == ' ' && (*tokens)->next)
+				else if (*expanded == ' ') //space left
 				{
-					if (((*tokens)->next->next && ((*tokens)->next->next->token->type != DQUOTE || (*tokens)->prev->prev->token->type != SQUOTE)))
+					if (((*tokens)->prev->token->type == SQUOTE || (*tokens)->prev->token->type == DQUOTE) \
+					&& ((*tokens)->prev->prev && ((*tokens)->prev->prev->token->type == (*tokens)->prev->token->type)))
 					{
-						// printf("here 5\n");
+						// printf("hna\n");
+						return (0);
+					}
+					if ((*tokens)->prev && ((*tokens)->prev->token->type != WSPACE && (*tokens)->prev->token->state == DEFAULT))
+					{
+						// printf("hna2\n");
 						return (1);
 					}
+				}
+				else if (expanded[ft_strlen(expanded) - 1] == ' ' && (*tokens)->next) //space right
+				{
+					// printf("dlkha\n");
+					// if ((*tokens)->next->next && ((*tokens)->next->next->token->type == WSPACE && (*tokens)->next->next->token->state == DEFAULT))
+						return (1);
 				}
 				else if ((!expanded || ft_strlen(expanded) == 0)  && (!(*tokens)->next || !(*tokens)->prev))
 				{
@@ -90,11 +92,11 @@ int	handle_word_redir(char **command, int *i, t_lexer **tokens, t_shell **shell)
 				}
 				else
 				{
-					// printf("%s\n", expanded)
+					// printf("%s\n", expanded);
 					char **split = ft_split(expanded, ' ');
 					if (args_count(split) == 0 || args_count(split) > 1 || !split[0] || !ft_strlen(split[0]))
 					{
-						// printf("here 7\n")
+						// printf("here 7\n");
 						return (1);
 					}
 				}
