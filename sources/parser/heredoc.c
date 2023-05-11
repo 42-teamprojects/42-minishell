7b45cec0a6e2c0400eca25edf5d37d1a7b1b4086
YusufLisawi <yusufisawi@gmail.com>
2023-05-11 16:37:18 +0100

    heredoc

--- sources/parser/heredoc.c
+++ sources/parser/heredoc.c
@@ -6,7 +6,7 @@
 /*   By: yelaissa <yelaissa@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 11:08:33 by yelaissa          #+#    #+#             */
-/*   Updated: 2023/05/11 11:36:27 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/05/11 14:45:59 by yelaissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,55 +42,41 @@ char *expand_variables_in_line(char *line, t_shell **shell)
     return expanded_line;
 }
 
-char *open_heredoc(t_lexer **tokens, t_shell **shell)
-{
-    int fd;
-    int i;
-    int flag;
-    char *line;
-    char *delimiter;
-    char *expanded_line;
+char **get_args(t_lexer **tokens, t_shell **shell, int *i, int *flag) {
     char **args;
 
-	i = 0;
-	args = (char **)malloc(sizeof(char *) * (args_len(*tokens, shell, WSPACE) + 1));
-	if (!args)
-		return (NULL);
-	flag = 0;
-	while ((*tokens))
-	{
-		if (is_word(*tokens))
-			handle_word(args, &i, tokens, shell, 0);
-		if (is_quote(*tokens))
-		{
-			flag = 1;
-			handle_quote(args, &i, tokens, shell, 0);
-		}
-		if ((*tokens)->token->type == WSPACE || !(*tokens)->next)
-			break ;
-		(*tokens) = (*tokens)->next;
-	}
-	args[i] = NULL;
-	if (!args[0])
-		return (NULL);
-	delimiter = args[0];
-    fd = open("/tmp/.ms_heredoc", O_CREAT | O_TRUNC | O_WRONLY, 0644);
-    if (fd < 0)
+    args = (char **)malloc(sizeof(char *) * (args_len(*tokens, shell, WSPACE) + 1));
+    if (!args)
         return (NULL);
-    while (1)
-    {
+    while ((*tokens)) {
+        if (is_word(*tokens))
+            handle_word(args, i, tokens, shell, 0);
+        if (is_quote(*tokens)) {
+            *flag = 1;
+            handle_quote(args, i, tokens, shell, 0);
+        }
+        if ((*tokens)->token->type == WSPACE || !(*tokens)->next)
+            break;
+        (*tokens) = (*tokens)->next;
+    }
+    args[*i] = NULL;
+    return args;
+}
+
+void write_heredoc(int fd, char *line, char *delimiter, t_shell **shell, int flag) {
+    char    *expanded_line;
+
+    while (1) {
         ft_putstr_fd("heredoc> ", 0);
         line = get_next_line(0);
         if (!line)
             break;
         line = ft_strtrim(line, "\n");
-        if (ft_strcmp(line, delimiter) == 0)
-        {
+        if (ft_strcmp(line, delimiter) == 0) {
             free(line);
             break;
         }
-        if (!flag)
-        {
+        if (!flag) {
             expanded_line = expand_variables_in_line(line, shell);
             free(line);
             line = expanded_line;
@@ -99,7 +85,26 @@ char *open_heredoc(t_lexer **tokens, t_shell **shell)
         write(fd, "\n", 1);
         free(line);
     }
+}
+
+char *open_heredoc(t_lexer **tokens, t_shell **shell) {
+    int     fd;
+    int     i;
+    int     flag;
+    char    *line;
+    char    **args;
+
+    i = 0;
+    flag = 0;
+    args = get_args(tokens, shell, &i, &flag);
+    line = NULL;
+    if (!args[0])
+        return (NULL);
+    fd = open("/tmp/.ms_heredoc", O_CREAT | O_TRUNC | O_WRONLY, 0644);
+    if (fd < 0)
+        return (NULL);
+    write_heredoc(fd, line, args[0], shell, flag);
+    free_array(args);
     close(fd);
     return (ft_strdup("/tmp/.ms_heredoc"));
 }
-
