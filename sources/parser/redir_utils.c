c9f01537e0b176e63709c86d013cfbf0b3b5096a
Youssef El Aissaoui <90868691+YusufLisawi@users.noreply.github.com>
2023-05-11 20:57:40 +0100

    99.23%

--- sources/parser/redir_utils.c
+++ sources/parser/redir_utils.c
@@ -6,7 +6,7 @@
 /*   By: yelaissa <yelaissa@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/11 16:49:50 by yelaissa          #+#    #+#             */
-/*   Updated: 2023/05/11 17:48:57 by yelaissa         ###   ########.fr       */
+/*   Updated: 2023/05/11 20:54:52 by yelaissa         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -43,28 +43,72 @@ int	handle_word_redir(char **command, int *i, t_lexer **tokens, t_shell **shell)
 			if (!expanded)
 			{
 				if (is_var_alone(*tokens))
+				{
 					return (1);
+				}
 				else
 					expanded = ft_strdup("");
 			}
 			else
 			{
+				if (ft_strlen(expanded) == 0 && !is_var_alone(*tokens))
+				{
+					t_lexer *tmp = (*tokens)->next;
+					while (tmp && is_quote(tmp))
+							tmp = tmp->next;
+					if (!tmp && (*tokens)->next && is_quote((*tokens)->next))
+						expanded = ft_strdup("");
+					t_lexer *tmp2 = (*tokens)->next;
+					while (tmp2 && ((tmp2->token->type == WSPACE || is_redir(tmp2))  && tmp2->token->state == DEFAULT) && is_quote(tmp2))
+							tmp2 = tmp2->prev;
+					if (!tmp2 && (*tokens)->next && is_quote((*tokens)->next))
+						expanded = ft_strdup("");
+					if ((*tokens)->prev && (*tokens)->prev->token->type != WSPACE && \
+						!is_redir((*tokens)->prev) && \
+							(*tokens)->prev->token->type != PIPE)
+						{
+							if (command[*i - 1])
+								command[*i - 1] = ft_strjoin_gnl(command[*i - 1], \
+									expanded);
+							else
+								command[(*i)++] = expanded;
+						}
+					else
+						command[(*i)++] = expanded;
+					return (0);
+				}
 				if (is_only_whitespace(expanded)) //space
 				{
-					if (!(*tokens)->next && !(*tokens)->prev)
+					t_lexer *tmp = (*tokens)->next;
+					while (tmp && is_quote(tmp))
+							tmp = tmp->next;
+					if (!tmp && (*tokens)->next && is_quote((*tokens)->next))
 						return (1);
+					t_lexer *tmp2 = (*tokens)->next;
+					while (tmp2 && ((tmp2->token->type == WSPACE || is_redir(tmp2))  && tmp2->token->state == DEFAULT) && is_quote(tmp2))
+							tmp2 = tmp2->prev;
+					if (!tmp2 && (*tokens)->next && is_quote((*tokens)->next))
+						return (1);
+					if (!(*tokens)->next && !(*tokens)->prev)
+					{
+						return (1);
+					}
 					if ((*tokens)->prev && ((*tokens)->prev->token->type == SQUOTE || (*tokens)->prev->token->type == DQUOTE) \
 					&& ((*tokens)->prev->prev && ((*tokens)->prev->prev->token->type == (*tokens)->prev->token->type)))
+					{
 						return (1);
-					if ((*tokens)->next && ((*tokens)->next->token->type == SQUOTE || (*tokens)->next->token->type == DQUOTE) \
-					&& ((*tokens)->next->next && ((*tokens)->next->next->token->type == (*tokens)->next->token->type)))
-						return (1);
+					}
 					if ((*tokens)->prev && ((*tokens)->next == NULL || ((*tokens)->next->token->type == WSPACE && (*tokens)->next->token->state == DEFAULT)))
 						return (0);
 				}
 				else if (*expanded == ' ' && ((*tokens)->prev && (*tokens)->prev->token->type != WSPACE && !is_redir((*tokens)->prev))) //space left
 				{
-					if ((command[*i - 1] && ft_strlen(command[*i - 1]) > 0 && !is_only_whitespace(command[*i - 1])) && ((*tokens)->prev->token->state == DEFAULT))
+					if (((*tokens)->prev->token->type == SQUOTE || (*tokens)->prev->token->type == DQUOTE) \
+						&& command[*i - 1] && ft_strlen(command[*i - 1]) > 0 && is_only_whitespace(command[*i - 1]))
+					{
+						return (1);
+					}
+					else if ((command[*i - 1] && ft_strlen(command[*i - 1]) > 0 && !is_only_whitespace(command[*i - 1])))
 					{
 						return (1);
 					}
@@ -74,28 +118,35 @@ int	handle_word_redir(char **command, int *i, t_lexer **tokens, t_shell **shell)
 					if (is_quote((*tokens)->next))
 					{
 						t_lexer *tmp = (*tokens)->next;
-						char *str = parse_quotes(&tmp, shell, 1);
-						if (str && ft_strlen(str) > 0)
+						while (tmp && is_quote(tmp))
+							tmp = tmp->next;
+						if (tmp && tmp->token->type == WORD)
 							return (1);
-						free(str);
+						if (tmp && tmp->prev && is_quote(tmp->prev))
+						{
+							tmp = tmp->prev;
+							char *str = parse_quotes(&tmp, shell, 1);
+							if (tmp->next != NULL || (tmp->next && (tmp->next->token->type == WSPACE && tmp->next->token->state == DEFAULT)))
+								return (1);
+							if (str && ft_strlen(str) > 0)
+								return (1);
+							free(str);
+						}
 					}
 					else if ((*tokens)->next->token->type == VAR)
 					{
 						char *str = ft_getenv(shell, (*tokens)->next->token->content + 1);
 						if (str && ft_strlen(str) > 0 && !is_only_whitespace(str))
+						{
 							return (1);
+						}
 					}
-				}
-				else if ((!expanded || ft_strlen(expanded) == 0)  && (!(*tokens)->next || !(*tokens)->prev))
-				{
-					return (1);
 				}
 				else
 				{
 					char **split = ft_split(expanded, ' ');
 					if (args_count(split) == 0 || args_count(split) > 1 || !split[0] || !ft_strlen(split[0]))
 					{
-						// printf("here 7\n");
 						return (1);
 					}
 				}
